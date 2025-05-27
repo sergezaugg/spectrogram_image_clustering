1e215f7a05efa19a5d539c5af940f1f87b6aa3a8
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-27 09:04:54 +0200

    misc

--- st_dashboard/page_ss.py
+++ st_dashboard/page_ss.py
@@ -1,8 +1,15 @@
 
 
 import streamlit as st
+import os
 from streamlit import session_state as ss
+
+st.write(ss['dapar']['feat_path'])
+st.write(ss['dapar']['imgs_path'])
+
+
+st.write(os.listdir(ss['dapar']['feat_path']))
+
 
 st.write('dapar', ss['dapar'])
 st.write('upar', ss['upar'])
-
