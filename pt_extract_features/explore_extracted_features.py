9d82287d73b412d8c8811cea2cb779001379de62
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-30 15:26:54 +0200

    improved explore features script

--- pt_extract_features/explore_extracted_features.py
+++ pt_extract_features/explore_extracted_features.py
@@ -1,6 +1,7 @@
 #-------------------------
 # Author : Serge Zaugg
-# check actual range of extracted features 
+# Description: Interactive script to visually explore distribution of feature vectors 
+# 
 #-------------------------
 
 import os 
@@ -10,21 +11,24 @@ from sklearn.model_selection import train_test_split
 
 featu_path = "./extracted_features"
 
-li = ["features_DenseNet121_denseblock3.npz", "features_MaxVit_T_blocks.3.npz", "features_vgg16_features.28.npz",
-      "features_ResNet50_layer1.npz", "features_ResNet50_layer2.npz", "features_ResNet50_layer3.npz",
+# explore full-sized feature vectors 
+li = [
+    "features_DenseNet121_denseblock3.npz", "features_MaxVit_T_blocks.3.npz", "features_vgg16_features.28.npz",
+    "features_ResNet50_layer1.npz", "features_ResNet50_layer2.npz", "features_ResNet50_layer3.npz",
     ]
 
 for file_name_in in li:
-    print(file_name_in)
-    npzfile_full_path = os.path.join(featu_path, file_name_in)
-    npzfile = np.load(npzfile_full_path)
+    npzfile = np.load(os.path.join(featu_path, file_name_in))
     X = npzfile['X']
-    # take only a few random samples to spped up plotly plots 
-    XS, _ = train_test_split(X, train_size=300, random_state=6666, shuffle=True)
-    XS.shape
-    fig = px.scatter(data_frame = XS, title = file_name_in)
-    fig.show()
+    XS, _ = train_test_split(X, train_size=200, random_state=6666, shuffle=True)
+    px.scatter(data_frame = XS.T, title = file_name_in).show()
 
         
-
+# explore dim-reduced features 
+for file_name_in in [a for a in os.listdir(featu_path) if 'dimred_' in a]:
+    npzfile = np.load(os.path.join(featu_path, file_name_in))
+    X = npzfile['X_red']
+    XS, _ = train_test_split(X, train_size=200, random_state=6666, shuffle=True)
+    px.scatter(data_frame = XS.T, title = file_name_in).show()
+    
 
