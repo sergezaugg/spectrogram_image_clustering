166b6d274c2b453347422e1699e1db9cb88b7915
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-30 16:26:13 +0200

    changed slider defaults of eps

--- st_dashboard/stmain.py
+++ st_dashboard/stmain.py
@@ -32,7 +32,7 @@ if 'upar' not in ss:
         'umap_n_neighbors' : 10,
         'umap_n_dims_red' : 8,
         'skip_umap' : False,
-        'dbscan_eps' : 0.447,
+        'dbscan_eps' : 0.5,
         'dbscan_min_samples' : 25,
         'exclude_non_assigned' : False
         }
