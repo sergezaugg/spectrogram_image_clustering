7ee3fadb00f1f296bb3f6c7c972912551a8c04f2
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-01 10:41:04 +0200

    made a super-short minimal main.py

---
+++ pt_extract_features/01_extract.py
@@ -0,0 +1,66 @@
+#--------------------             
+# Author : Serge Zaugg
+# Description : A script to keep track of feature extraction history
+# Mainly to apply pt_extract_features.utils_ml.FeatureExtractor with several models and params
+#--------------------
+
+import os 
+import numpy as np
+import datetime
+import torch
+from pt_extract_features.utils_ml import FeatureExtractor
+torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+#-------------------------
+# set paths   
+image_path = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
+featu_path = "./extracted_features"
+
+
+#-------------------------
+# fex
+
+fe = FeatureExtractor(model_tag = "ResNet50")
+fe.eval_nodes
+fe.create("layer1.2.conv3")
+fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = 800)
+
+# fe = FeatureExtractor(model_tag = "ResNet50")
+# fe.eval_nodes
+# fe.create("layer2.3.conv3")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = 800)
+
+# fe = FeatureExtractor(model_tag = "ResNet50")
+# fe.eval_nodes
+# fe.create("layer3.5.conv3")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = 10)
+
+# fe = FeatureExtractor(model_tag = "vgg16")
+# fe.eval_nodes
+# fe.create("features.28")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = 800)
+
+# fe = FeatureExtractor(model_tag = "DenseNet121")
+# fe.eval_nodes
+# fe.create("features.denseblock3")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = 800)
+
+# fe = FeatureExtractor(model_tag = "MaxVit_T")
+# fe.eval_nodes
+# fe.create("blocks.3.layers.1.layers.MBconv.layers.conv_c")
+# fe.extract(image_path, freq_pool = 1, batch_size = 16, n_batches = 800)
+
+
+#-------------------------
+# save as npz
+print(fe.X.shape, fe.N.shape)
+# print(fe.X_li, fe.N_li)
+tstmp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_")
+out_name = os.path.join(featu_path, tstmp + 'features_' + fe.model_tag + fe.fex_tag + '.npz')
+np.savez(file = out_name, X = fe.X, N = fe.N)
+
+
+
+
+
