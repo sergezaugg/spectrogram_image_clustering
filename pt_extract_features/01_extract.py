fabe1e746652fb01ebf116bd51fa460a23a38ec4
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-01 16:45:48 +0200

    misc

--- pt_extract_features/01_extract.py
+++ pt_extract_features/01_extract.py
@@ -32,15 +32,15 @@ n_batches = 800 # prod
 # fe.create("layer2.3.conv3")
 # fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
 
-fe = FeatureExtractor(model_tag = "ResNet50")
-fe.eval_nodes
-fe.create("layer3.5.conv3")
-fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
-
-# fe = FeatureExtractor(model_tag = "vgg16")
+# fe = FeatureExtractor(model_tag = "ResNet50")
 # fe.eval_nodes
-# fe.create("features.28")
+# fe.create("layer3.5.conv3")
 # fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
+
+fe = FeatureExtractor(model_tag = "vgg16")
+fe.eval_nodes
+fe.create("features.28")
+fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
 
 # fe = FeatureExtractor(model_tag = "DenseNet121")
 # fe.eval_nodes
