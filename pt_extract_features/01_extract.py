1dfd8bd26e8b3860e191a4a5d926a4538504a425
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-05 12:55:11 +0200

    a few edits in pt backend

--- pt_extract_features/01_extract.py
+++ pt_extract_features/01_extract.py
@@ -1,6 +1,6 @@
 #--------------------             
 # Author : Serge Zaugg
-# Description : A script to keep track of feature extraction history
+# Description : A script to keep track and reproduce feature extraction history
 # Mainly to apply pt_extract_features.utils_ml.FeatureExtractor with several models and params
 #--------------------
 
@@ -14,19 +14,16 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 #-------------------------
 # set paths   
-image_path = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
-# image_path = "D:/xc_real_projects/xc_parus_01/xc_spectrograms"
+# image_path = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
+image_path = "D:/xc_real_projects/xc_parus_01/xc_spectrograms"
 featu_path = "./extracted_features"
 # n_batches = 10 # dev
-n_batches = 800 # prod
+n_batches = 800000 # prod
 
 #-------------------------
 # fex
 
-# fe = FeatureExtractor(model_tag = "ResNet50")
-# fe.eval_nodes
-# fe.create("layer1.2.conv3")
-# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
+# PRIMARY
 
 # fe = FeatureExtractor(model_tag = "ResNet50")
 # fe.eval_nodes
@@ -38,20 +35,29 @@ n_batches = 800 # prod
 # fe.create("layer3.5.conv3")
 # fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
 
+fe = FeatureExtractor(model_tag = "MaxVit_T")
+fe.eval_nodes
+fe.create("blocks.3.layers.1.layers.MBconv.layers.conv_c")
+fe.extract(image_path, freq_pool = 1, batch_size = 16, n_batches = n_batches)
+
+
+
+# SECONDARY
+
+# fe = FeatureExtractor(model_tag = "ResNet50")
+# fe.eval_nodes
+# fe.create("layer1.2.conv3")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
+
+# fe = FeatureExtractor(model_tag = "DenseNet121")
+# fe.eval_nodes
+# fe.create("features.denseblock3")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
+
 # fe = FeatureExtractor(model_tag = "vgg16")
 # fe.eval_nodes
 # fe.create("features.28")
 # fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
-
-fe = FeatureExtractor(model_tag = "DenseNet121")
-fe.eval_nodes
-fe.create("features.denseblock3")
-fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches)
-
-# fe = FeatureExtractor(model_tag = "MaxVit_T")
-# fe.eval_nodes
-# fe.create("blocks.3.layers.1.layers.MBconv.layers.conv_c")
-# fe.extract(image_path, freq_pool = 1, batch_size = 16, n_batches = n_batches)
 
 
 #-------------------------
--- pt_extract_features/03_explore.py
+++ pt_extract_features/03_explore.py
@@ -14,6 +14,7 @@ featu_path = "./extracted_features"
 for file_name_in in [a for a in os.listdir(featu_path) if 'full_features_' in a]:
     npzfile = np.load(os.path.join(featu_path, file_name_in))
     X = npzfile['X']
+    print(X.shape)
     XS, _ = train_test_split(X, train_size=150, random_state=6666, shuffle=True)
     px.scatter(data_frame = XS.T, title = file_name_in).show()
     
@@ -21,6 +22,7 @@ for file_name_in in [a for a in os.listdir(featu_path) if 'full_features_' in a]
 for file_name_in in [a for a in os.listdir(featu_path) if 'dimred_' in a]:
     npzfile = np.load(os.path.join(featu_path, file_name_in))
     X = npzfile['X_red']
+    print(X.shape)
     XS, _ = train_test_split(X, train_size=150, random_state=6666, shuffle=True)
     px.scatter(data_frame = XS.T, title = file_name_in).show()
     
