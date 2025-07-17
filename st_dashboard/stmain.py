c34eb22bac660d9821a746fead618baf82de40cc
Serge Zaugg <serge.zaugg@gmail.com>
2025-07-17 16:49:32 +0200

    inc to v0.9.15

--- st_dashboard/stmain.py
+++ st_dashboard/stmain.py
@@ -69,7 +69,7 @@ with st.sidebar:
     st.text("")
     c1,c2=st.columns([80,200])
     c1.image(image='pics/z_logo_red.png', width=65)
-    c2.markdown(''':primary[v0.9.14-exp1 (beta)]  
+    c2.markdown(''':primary[v0.9.15 (beta)]  
     :primary[Created by]
     :primary[[Serge Zaugg](https://www.linkedin.com/in/dkifh34rtn345eb5fhrthdbgf45/)]    
     :primary[[Pollito-ML](https://github.com/sergezaugg)]
